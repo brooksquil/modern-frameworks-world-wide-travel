81e2401b71110fe35c2afbb1d5e24d21a4afaf6b
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-09-08 13:43:00 -0500

    Finished exercise

--- app/app.js
+++ app/app.js
@@ -1,1 +1,12 @@
 "use strict";
+
+var app = angular.module("wwt", ['ngRoute']);
+
+app.config(function($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'partials/display.html',
+        controller: 'BookCtrl'
+    }).
+    otherwise('/');
+});
